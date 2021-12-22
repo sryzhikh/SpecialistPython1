a1f9e03882a9f74094163f95d2933669a9af802d
sryzhikh <96463832+sryzhikh@users.noreply.github.com>
2021-12-22 20:03:11 +0400

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,13 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+mux_len_names = len(names[0])
+for name in names:
+    if len(name) > mux_len_names:
+        mux_len_names = len(name)
+# print(mux_len_names)
+
+for long_name in names:
+    if len(long_name) == mux_len_names:
+        print(long_name)
