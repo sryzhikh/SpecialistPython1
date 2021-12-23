3df3c0119a624fa71f92c14ee249121bbd9838fd
sryzhikh <96463832+sryzhikh@users.noreply.github.com>
2021-12-23 20:51:08 +0400

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -11,3 +11,13 @@ def lucky_ticket(ticket_number):
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+
+def lucky_ticket(lucky_ticket):
+    lucky_ticket = str(lucky_ticket)
+    if len(lucky_ticket) != 6:
+        print("Это не билет", end=" ")
+        return False
+    if int(lucky_ticket[0])+int(lucky_ticket[1])+int(lucky_ticket[2]) == int(lucky_ticket[3])+int(lucky_ticket[4])+int(lucky_ticket[5]):
+        return True
+    else:
+        return False
