8a1a1abc37e7a624b6e513751fd3495971f927aa
sryzhikh <96463832+sryzhikh@users.noreply.github.com>
2021-12-23 15:37:50 +0400

    Update 03_task_func.py

--- Module4/practice/funcs_part1/03_task_func.py
+++ Module4/practice/funcs_part1/03_task_func.py
@@ -10,3 +10,7 @@ def distance(x1, y1, x2, y2):
 print(distance(2, 4, 2, 9))
 print(distance(12, 8, 12, -9))
 print(distance(23, 0, 15, 32))
+
+def distance(x1, y1, x2, y2):
+    summ = round(((x2 - x1)**2 + (y2-y1)**2)**0.5)
+    return summ
